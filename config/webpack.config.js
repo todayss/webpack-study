0b8926a639d58513ff03744fe43aab790a19b75b
wanxiang <853624405@qq.com>
2020-12-18 00:06:48 +0800

    feature: webpack-dev-server添加

--- config/webpack.config.js
+++ config/webpack.config.js
@@ -1,5 +1,6 @@
 const path = require('path')
 const { CleanWebpackPlugin } = require('clean-webpack-plugin')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
 
 module.exports = {
   entry: './src/index.js',
@@ -11,11 +12,20 @@ module.exports = {
     rules: []
   },
   plugins: [
-    new CleanWebpackPlugin()
+    new CleanWebpackPlugin(),
+    new HtmlWebpackPlugin({
+      template: path.resolve(__dirname, '../public/index.html'),
+      filename: 'index.html'
+    })
   ],
   mode: 'development',
   resolve: {
     extensions: ['.js', '.jsx', '.ts', '.tsx']
   },
-  devtool: 'inline-source-map'
+  devtool: 'inline-source-map',
+  devServer: {
+    port: 9999,
+    open: true,
+    hot: true
+  }
 }
